aec8febf6097d3478414213706b016a6c7190f54
syedmisbahurrehman <syedmisbah588@gmail.com>
2024-02-12 15:16:56 +0500

    Route Groups

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
- import { fetchRevenue , fetchLatestInvoices,fetchCardData} from '../lib/data';
+ import { fetchRevenue , fetchLatestInvoices,fetchCardData} from '../../lib/data';
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
