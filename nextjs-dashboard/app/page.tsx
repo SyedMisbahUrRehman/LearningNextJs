9ddabbbb969aaea9e82361e2530569aade51c359
syedmisbahurrehman <syedmisbah588@gmail.com>
2024-02-12 10:35:49 +0500

    added images

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from './ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -30,6 +31,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+           <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="md:hidden block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
         </div>
       </div>
     </main>
