c4e798a33b0ed0e9bb30db53ea793e4ff1397032
syedmisbahurrehman <syedmisbah588@gmail.com>
2024-02-14 14:45:15 +0500

    changed ownership to mohsin tech

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
             className={styles.shape}
           />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased `}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Mohsin Tech.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
